49cbdc79181a7313ffa0cf4a4316891444715f25
Viviana Feijoo <vivfeijoo@gmail.com>
2025-05-21 11:12:35 +0000

    css

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from 'react';
+import '../../styles/index.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && inputValue.trim() !== '') {
+      setTasks([...tasks, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  const removeTask = (indexToRemove) => {
+    const updatedTasks = tasks.filter((_, index) => index !== indexToRemove);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="page-wrapper">
+      <h1 className="app-title">todos</h1>
+
+      <div className="paper-stack">
+        <div className="sheet-3"></div>
+        <div className="sheet-2"></div>
+        <div className="sheet-1">
+          <div className="todo-container">
+            <input
+            className="todo-input"
+            type="text"
+            placeholder="What needs to be done?"
+            onKeyDown={handleKeyDown}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            />
+
+            <ul className="task-list">
+              {tasks.map((task, index) => (
+                <li key={index}>
+                  {task}
+                  <button onClick={() => removeTask(index)}>✖</button>
+                </li>
+              ))}
+            </ul>
+            <div className="task-counter">
+              {tasks.length} item{tasks.length !== 1 ? 's' : ''} left
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
